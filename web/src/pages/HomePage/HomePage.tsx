6e5eac6c9fb7192195a1475617fb0554eb713eea
Milesscott <milesbscott@gmail.com>
2023-02-24 13:59:59 +0000

    More changes to homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -11,35 +11,36 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <Stack align="center">
-        <Image
-          borderRadius="full"
-          boxSize="150px"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-      </Stack>
-
-      <Text noOfLines={[1, 2, 3]}>
-        "The quick brown fox jumps over the lazy dog" is an English-language
-        pangram—a sentence that contains all of the letters of the English
-        alphabet. Owing to its existence, Chakra was created.
-      </Text>
-
-      <Stack
-        direction="column"
-        spacing={2}
-        align="center"
-        justify={'space-evenly'}
-      >
-        <Button colorScheme="purple" variant="solid" size={'lg'}>
-          Sign in
-        </Button>
-        <Button colorScheme="" variant="outline">
-          Create account
-        </Button>
-      </Stack>
+      <Box backgroundColor={'purple.600'}>
+        {' '}
+        <Stack align="center">
+          <Image
+            m={2}
+            borderRadius="2xl"
+            boxSize="500px"
+            src="https://bit.ly/dan-abramov"
+            alt="Dan Abramov"
+          />
+        </Stack>
+        <Text noOfLines={[1, 2, 3]}>
+          "The quick brown fox jumps over the lazy dog" is an English-language
+          pangram—a sentence that contains all of the letters of the English
+          alphabet. Owing to its existence, Chakra was created.
+        </Text>
+        <Stack
+          direction="column"
+          spacing={2}
+          align="center"
+          justify={'space-evenly'}
+        >
+          <Button colorScheme="purple" variant="solid" size={'lg'}>
+            Sign in
+          </Button>
+          <Button colorScheme="" variant="outline">
+            Create account
+          </Button>
+        </Stack>
+      </Box>
     </>
   )
 }
