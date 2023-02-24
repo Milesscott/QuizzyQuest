c31d4e2f8e628df4c72712d31706e955399c5f38
Milesscott <milesbscott@gmail.com>
2023-02-24 12:30:06 +0000

    Homepage Styles
    Started to style homepage components

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { Image } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
 
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
@@ -11,19 +12,34 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <Box boxSize="sm">
-        <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
-      </Box>
+      <Stack align="center">
+        <Image
+          borderRadius="full"
+          boxSize="150px"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+      </Stack>
+
       <Text noOfLines={[1, 2, 3]}>
         "The quick brown fox jumps over the lazy dog" is an English-language
         pangram—a sentence that contains all of the letters of the English
         alphabet. Owing to its existence, Chakra was created.
       </Text>
 
-      <Button colorScheme="blue" size="lg">
-        Button
-      </Button>
-      <Button colorScheme="blue">Button</Button>
+      <Stack
+        direction="column"
+        spacing={2}
+        align="center"
+        justify={'space-evenly'}
+      >
+        <Button colorScheme="purple" variant="solid" size={'lg'}>
+          Sign in
+        </Button>
+        <Button colorScheme="" variant="outline">
+          Create account
+        </Button>
+      </Stack>
     </>
   )
 }
