db6212d52206c91e89a045a97f33fa1be359f394
Milesscott <milesbscott@gmail.com>
2023-03-10 13:31:12 +0000

    New Base
    New branches will go from here

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,47 +1,78 @@
-import { Image } from '@chakra-ui/react'
-import { Box } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { Stack } from '@chakra-ui/react'
+import { Box, Heading, Container, Text, Button, Stack } from '@chakra-ui/react'
+import Navbar, { WithSubnavigation } from 'web/src/components/Navbar/Navbar'
 
-import { MetaTags } from '@redwoodjs/web'
-
-const HomePage = () => {
+import Footer, { ListHeader } from 'src/components/Footer/Footer'
+export default function CallToActionWithAnnotation() {
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
-      <Box backgroundColor={'purple.600'}>
-        {' '}
-        <Stack align="center">
-          <Image
-            m={2}
-            borderRadius="2xl"
-            boxSize="500px"
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-          />
-        </Stack>
-        <Text noOfLines={[1, 2, 3]}>
-          "The quick brown fox jumps over the lazy dog" is an English-language
-          pangram—a sentence that contains all of the letters of the English
-          alphabet. Owing to its existence, Chakra was created.
-        </Text>
+      <header>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </header>
+      <WithSubnavigation></WithSubnavigation>
+
+      <Container maxW={'3xl'}>
         <Stack
-          direction="column"
-          spacing={2}
-          align="center"
-          justify={'space-evenly'}
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
         >
-          <Button colorScheme="purple" variant="solid" size={'lg'}>
-            Sign in
-          </Button>
-          <Button colorScheme="" variant="outline">
-            Create account
-          </Button>
+          <Navbar></Navbar>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+          >
+            Join the Quest for Knowledge with <br />
+            <Text as={'span'} color={'purple.500'}>
+              Quizzy Quest!
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Monetize your content by charging your most loyal readers and reward
+            them loyalty points. Give back to your loyal readers by granting
+            them access to your pre-releases and sneak-peaks.
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}
+          >
+            <Button
+              colorScheme={'green'}
+              bg={'purple.500'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'purple.500',
+              }}
+            >
+              Sign Up
+            </Button>
+            <Button
+              colorScheme={'green'}
+              bg={'purple.500'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'purple.500',
+              }}
+            >
+              Sighn In
+            </Button>
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              Learn more
+            </Button>
+          </Stack>
         </Stack>
-      </Box>
+      </Container>
+      <Footer></Footer>
+      <ListHeader></ListHeader>
     </>
   )
 }
-
-export default HomePage
